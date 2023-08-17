97b50fea6fcac5cd207ed5b206043f49759ddb7d
0roc4n <76811555+0roc4n@users.noreply.github.com>
2023-08-18 00:02:55 +0800

    mssql database

---
+++ qlda.sql
@@ -0,0 +1,85 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 17, 2023 at 06:01 PM
+-- Server version: 8.0.32
+-- PHP Version: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `qlda`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `status`
+--
+
+CREATE TABLE `status` (
+  `status` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_vietnamese_ci;
+
+--
+-- Dumping data for table `status`
+--
+
+INSERT INTO `status` (`status`) VALUES
+(1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stat_tbl`
+--
+
+CREATE TABLE `stat_tbl` (
+  `id` int NOT NULL,
+  `status` varchar(100) DEFAULT NULL,
+  `is_on` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `stat_tbl`
+--
+
+INSERT INTO `stat_tbl` (`id`, `status`, `is_on`) VALUES
+(1, '0', 'false'),
+(2, '0', 'true'),
+(3, '0', 'false');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `stat_tbl`
+--
+ALTER TABLE `stat_tbl`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `stat_tbl`
+--
+ALTER TABLE `stat_tbl`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
