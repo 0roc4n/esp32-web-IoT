f0bafbf3c3141356be0eab99a6eed2378f03da2e
0roc4n <76811555+0roc4n@users.noreply.github.com>
2023-08-17 23:55:42 +0800

    get status from database

---
+++ Getstatus.php
@@ -0,0 +1,31 @@
+<?php
+
+$server 	= "localhost";	// Change this to correspond with your database port
+$username 	= "root";			// Change if use webhost online
+$password 	= "root";
+$DB 		= "qlda";			// database name
+
+
+$conn = new mysqli($server, $username, $password,$DB);		// Check database connection
+	if ($conn->connect_error) 
+	{
+		//die("Connection failed: " . $conn->connect_error);
+	} 
+	
+	// $query ="SELECT * from status";					// Select all data in table "status"
+	// $result = $conn->query($query);
+	
+	// 	while($row = $result->fetch_assoc()) 
+	// 	{
+	// 		echo $row["status"];					// Echo data , equivalent with send data to esp
+	// 	}
+
+	//separate
+	$query2 ="SELECT * from stat_tbl";					// Select all data in table "status"
+	$result2 = $conn->query($query2);
+	
+		while($row2 = $result2->fetch_assoc()) 
+		{
+			echo $row2["id"]."". $row2["is_on"];				// Echo data , equivalent with send data to esp
+		}
+?>
