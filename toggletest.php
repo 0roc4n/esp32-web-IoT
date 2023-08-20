93f0ffab3ab214d88ed0c819355b9c67c9c8eb14
0roc4n <76811555+0roc4n@users.noreply.github.com>
2023-08-20 22:54:03 +0800

    active status

--- toggletest.php
+++ toggletest.php
@@ -13,37 +13,11 @@
 <html>
 <head>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <style>
-        /* Add some basic styling to the toggle button */
-        .toggle-button {
-            width: 50px;
-            height: 25px;
-            background-color: gray;
-            border-radius: 12.5px;
-            display: flex;
-            align-items: center;
-            cursor: pointer;
-        }
-        
-        .toggle-indicator {
-            width: 20px;
-            height: 20px;
-            background-color: white;
-            border-radius: 50%;
-            margin-left: 2.5px;
-            transition: margin-left 0.2s;
-        }
-        .toggled {
-            background-color: blue;
-        }
-    </style>
+
 </head>
 <body>
     
-    <div class="toggle-button" onclick="toggle()">
-        <div class="toggle-indicator" id="toggle-indicator"></div>
-    </div>
-    <input type="hidden" id="toggle-value" value="0">
+   
     <div class="container">
         <table class="table">
     
@@ -85,21 +59,5 @@
     
     </div>
     
-    <script>
-        function toggle() {
-            var indicator = document.getElementById("toggle-indicator");
-            var valueField = document.getElementById("toggle-value");
-            
-            if (indicator.style.marginLeft === "2.5px") {
-                indicator.style.marginLeft = "27.5px";
-                valueField.value = "1";
-                indicator.parentElement.classList.add("toggled");
-            } else {
-                indicator.style.marginLeft = "2.5px";
-                valueField.value = "0";
-                indicator.parentElement.classList.remove("toggled");
-            }
-        }
-    </script>
 </body>
 </html>
