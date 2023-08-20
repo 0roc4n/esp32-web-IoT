02d0c78a9ed8032ea0e17e2b6993363a90a0cde2
James Esparrago <76811555+0roc4n@users.noreply.github.com>
2023-08-20 21:06:31 +0800

    Add files via upload

--- con.php
+++ con.php
@@ -1,1 +1,13 @@
-
+<?php
+        $server 	= "localhost";	// Change this to correspond with your database port
+        $username 	= "root";			// Change if use webhost online
+        $password 	= "root";
+        $DB 		= "qlda";			// database name
+        
+        
+        $update = new mysqli($server, $username, $password, $DB);	// Check database connection
+                    if ($update->connect_error) {
+                        die("Connection failed: " . $update->connect_error);
+                    } 
+            
+?>
