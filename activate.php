734eac8bfdcf23312ec0d82aa2a5186dddc5418e
0roc4n <76811555+0roc4n@users.noreply.github.com>
2023-08-20 20:53:55 +0800

    module to activate

---
+++ activate.php
@@ -0,0 +1,23 @@
+<?php
+    include 'con.php';
+
+    $id = $_GET['id'];
+    $status = $_GET['status'];
+
+    $updatestat = "UPDATE ledstat SET status='$status' WHERE ledID='$id'";
+    mysqli_query($update, $updatestat);
+    header("location:toggletest.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+    
+</body>
+</html>
