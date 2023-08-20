e7a2fab8fe0859013e375324a47ea012c0ddad07
0roc4n <76811555+0roc4n@users.noreply.github.com>
2023-08-20 20:54:30 +0800

    updated code for arduino(esp32)

--- arduCode.ino
+++ arduCode.ino
@@ -4,13 +4,18 @@
 // WiFi parameters
 const char* ssid = "Connect";
 const char* password = "connect123";
+bool ledState23 = false;
+bool ledState22 = false;
+bool ledState1 = false;
 HTTPClient http;
 
 // host to send data
-const char* host = "http://192.168.159.238/esp32/Getstatus.php";
+const char* host = "http://192.168.38.238/esp32/Getstatus.php";
 
 void setup() {
   pinMode(23, OUTPUT);
+  pinMode(22, OUTPUT);
+  pinMode(21, OUTPUT);
   Serial.begin(9600);
   Serial.print("Connecting to ");
   Serial.println(ssid);
@@ -28,30 +33,55 @@ void setup() {
 }
 
 void loop() {
+  String payload;
   if (WiFi.status() == WL_CONNECTED) {
     http.begin(host);
     int httpCode = http.GET();
-    if (httpCode > 0) {
-      if (httpCode == HTTP_CODE_OK) {
-        String payload = http.getString();
-        Serial.println(payload);
-        if (payload == "112030") {
-          digitalWrite(23, HIGH); // if data == 1 -> LED ON
-        } else if (payload == "102030") {
-          digitalWrite(23, LOW); // if data == 0 -> LED OFF
-        }else if (payload == "102130") {
-          digitalWrite(23, HIGH); // if data == 0 -> LED OFF
-        }else if (payload == "102030") {
-          digitalWrite(23, LOW); // if data == 0 -> LED OFF
-        }else if (payload == "102031") {
-          digitalWrite(23, HIGH); // if data == 0 -> LED OFF
-        }else if (payload == "102030") {
-          digitalWrite(23, LOW); // if data == 0 -> LED OFF
-        }
-        
+    
+    if (httpCode > 0 && httpCode == HTTP_CODE_OK) {
+      payload = http.getString();
+      Serial.println(payload);
+      
+      if (payload == "112030") {
+        setLEDStates(true, false, false); // Turn on LED 23
+      }if (payload == "102030") {
+        setLEDStates(false, false, false); // Turn off all LEDs
+      }if (payload == "102130") {
+        setLEDStates(false, true, false); // Turn on LED 22
+      }if (payload == "102031") {
+        setLEDStates(false, false, true); // Turn on LED 1
+      }if (payload == "112233") {
+        setLEDStates(true, true, true); // Turn on all LEDs
+      }if (payload == "112230") {
+        setLEDStates(true, true, false); // Turn on LED 23 and 22
+      } else if (payload == "112033") {
+        setLEDStates(true, false, true); // Turn on LED 23 and 1
+      }if (payload == "102233") {
+        setLEDStates(false, true, true); // Turn on LED 22 and 1
       }
     }
+    
     http.end();
   }
+  //digitalWrite(21, HIGH);
+}
+
+void setLEDStates(bool state23, bool state22, bool state1) {
+  if (ledState23 != state23) {
+    ledState23 = state23;
+    digitalWrite(23, state23 ? HIGH : LOW);
+    delay(50);
+  }
   
+  if (ledState22 != state22) {
+    ledState22 = state22;
+    digitalWrite(22, state22 ? HIGH : LOW);
+    delay(50);
+  }
+  
+  if (ledState1 != state1) {
+    ledState1 = state1;
+    digitalWrite(21, state1 ? HIGH : LOW);
+    delay(50);
+  }
 }
