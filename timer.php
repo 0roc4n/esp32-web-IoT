b4dcddaa22c83a4b9e2f802a518ea849d9f0a175
0roc4n <76811555+0roc4n@users.noreply.github.com>
2023-08-21 23:20:08 +0800

    Scheduled On/Off of LEDs

---
+++ timer.php
@@ -0,0 +1,43 @@
+<?php
+include 'conn.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $selectedDatetime = $_POST['datetime'];
+
+    // Convert the datetime string to a DateTime object
+    $dateTime = new DateTime($selectedDatetime);
+
+    // Perform further processing with the $dateTime object
+    $formattedDatetime = $dateTime->format('Y-m-d H:i:s');
+
+    //echo "You selected: " . $formattedDatetime;
+    $sql = "INSERT INTO timer (datetime_value, is_active) VALUES ('$formattedDatetime', '1')";
+
+    if ($update->query($sql) === TRUE) {
+        echo "Datetime value inserted successfully!";
+    } else {
+        echo "Error: " . $sql . "<br>" . $update->error;
+    }
+
+    // Close the database connection
+    $update->close();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Timer</title>
+</head>
+<body>
+    <div class="container">
+        <form method="post" action="">
+            <label for="datetime">Select a Date and Time:</label>
+            <input type="datetime-local" id="datetime" name="datetime">
+            <button type="submit">Submit</button>
+        </form>
+</body>
+</html>
+
