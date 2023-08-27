51d3dd35dc6892c50c8c58bac0090b8134ecd3e6
0roc4n <76811555+0roc4n@users.noreply.github.com>
2023-08-27 23:27:03 +0800

    Update Arduino with TH upload

--- arduCode.ino
+++ arduCode.ino
@@ -16,12 +16,14 @@ HTTPClient http;
 
 // host to send data
 const char* host = "http://192.168.47.238/esp32/Getstatus.php";
-const char* host2 = "http://192.168.47.238";
+const char* host2 = "192.168.47.238";
 
 
 void setup() {
+  Serial.println("DHT11 Output!");
+  dht.begin();
   pinMode(23, OUTPUT);
-  pinMode(22, OUTPUT);
+  
   pinMode(21, OUTPUT);
   
   Serial.begin(9600);
@@ -72,65 +74,61 @@ void loop() {
 
     http.end();
   
-
-
-  // --------------------------------------
-  //try to put the whole dht11 upload to http begin wiht wifi connected
-  //alway s check id the wifi is connected and the http begin
-  //digitalWrite(21, HIGH);
+  
+  }
   float temperature = dht.readTemperature();
-  float humidity = dht.readHumidity();
-  if (isnan(temperature) || isnan(humidity)) {
-    Serial.println("Failed to read DHT11");
-  } else {
-    Serial.print("Humidity: ");
-    Serial.print(humidity);
-    Serial.print(" %\t");
-    Serial.print("Temperature: ");
-    Serial.print(temperature);
-    Serial.println(" *C");
-    delay(3000);
-  }
+    float humidity = dht.readHumidity();
 
-  Serial.print("connecting to ");
-  Serial.println(host2);
-
-  // Use WiFiClient class to create TCP connections
-  WiFiClient client;
-  const int httpPort = 8080;
-  if (!client.connect(host2, httpPort)) {
-    Serial.println("connection failed");
-    return;
-  }
-
-
-
-
-  // This will send the request to the server
-  client.print(String("GET http://192.168.47.238/esp32-web-iot/connection.php?") +
-               ("&temperature=") + temperature +
-               ("&humidity=") + humidity +
-               " HTTP/1.1\r\n" +
-               "Host: " + host2 + "\r\n" +
-               "Connection: close\r\n\r\n");
-  unsigned long timeout = millis();
-  while (client.available() == 0) {
-    if (millis() - timeout > 1000) {
-      Serial.println(">>> Client Timeout !");
-      client.stop();
-      return;
+    if (isnan(temperature) || isnan(humidity)) {
+        Serial.println("Failed to read DHT11");
+    } else {
+        Serial.print("Humidity: ");
+        Serial.print(humidity);
+        Serial.print(" %\t");
+        Serial.print("Temperature: ");
+        Serial.print(temperature);
+        Serial.println(" *C");
+        delay(3000);
     }
-  }
 
-  // Read all the lines of the reply from server and print them to Serial
-  while (client.available()) {
-    String line = client.readStringUntil('\r');
-    Serial.print(line);
+    Serial.print("connecting to ");
+    Serial.println(host2);
 
-  }
+    WiFiClient client;
+    const int httpPort = 80; // Use port 80 for HTTP
+
+    if (!client.connect(host2, httpPort)) {
+        Serial.println("connection failed");
+        return;
+    }
+
+    String request = "GET /esp32-web-iot/connection.php";
+    request += "?temperature=" + String(temperature);
+    request += "&humidity=" + String(humidity);
+    request += " HTTP/1.1\r\n";
+    request += "Host: " + String(host2) + "\r\n";
+    request += "Connection: close\r\n\r\n";
+
+    client.print(request);
+
+    unsigned long timeout = millis();
+    while (client.available() == 0) {
+        if (millis() - timeout > 5000) { // Increased timeout to 5 seconds
+            Serial.println(">>> Client Timeout !");
+            client.stop();
+            return;
+        }
+    }
+
+    while (client.available()) {
+        String line = client.readStringUntil('\r');
+        Serial.print(line);
+    }
+
+    Serial.println();
+    Serial.println("closing connection");
 }
 
-}
 
 void setLEDStates(bool state23, bool state22, bool state1) {
   if (ledState23 != state23) {
