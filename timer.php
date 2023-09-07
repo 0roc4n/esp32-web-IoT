41fbf5e3c22aa9113f20455c9c7b122c79473afb
0roc4n <76811555+0roc4n@users.noreply.github.com>
2023-09-07 23:45:27 +0800

    Select led

--- timer.php
+++ timer.php
@@ -1,23 +1,30 @@
 <?php
+session_start();
 include 'conn.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $selectedDatetime = $_POST['datetime'];
-        
+    if (isset($_POST['led_set'])) {
+        $led_set = $_POST['led_set'];
+        $leds = implode(",",$led_set);
+        echo $leds;
+        $_SESSION['selectedValue'] = $_POST['led_set'];
+    
 
     // Convert the datetime string to a DateTime object
-    $dateTime = new DateTime($selectedDatetime);
+        $dateTime = new DateTime($selectedDatetime);
 
-    // Perform further processing with the $dateTime object
-    $formattedDatetime = $dateTime->format('Y-m-d H:i:s');
+        // Perform further processing with the $dateTime object
+        $formattedDatetime = $dateTime->format('Y-m-d H:i:s');
 
-    //echo "You selected: " . $formattedDatetime;
-    $sql = "INSERT INTO timer (datetime_value, is_active) VALUES ('$formattedDatetime', '1')";
+        //echo "You selected: " . $formattedDatetime;
+        $sql = "INSERT INTO timer (datetime_value, is_active, choose_led) VALUES ('$formattedDatetime', '1', '$leds')";
 
-    if ($update->query($sql) === TRUE) {
-        echo "Datetime value inserted successfully!";
-    } else {
-        echo "Error: " . $sql . "<br>" . $update->error;
+        if ($update->query($sql) === TRUE) {
+            echo "Datetime value inserted successfully!";
+        } else {
+            echo "Error: " . $sql . "<br>" . $update->error;
+        }
     }
 
     // Close the database connection
@@ -30,6 +37,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Timer</title>
 </head>
 <body>
@@ -38,12 +46,23 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
             <label for="datetime">Select a Date and Time:</label>
             <div class="chck">
-                <label for="checkbox">Led 1</label>
-                <input type="checkbox" name="choose_led" value="led1">
-                <label for="checkbox">Led 2</label>
-                <input type="checkbox" name="choose_led" value="led1">
-                <label for="checkbox">Led 3</label>
-                <input type="checkbox" name="choose_led" value="led1">
+                <!-- <select class="form-select" aria-label="Default select example" name="led_set">
+                    <option value="LED1">One</option>
+                    <option value="LED2">Two</option>
+                    <option value="LED3">Three</option>
+                </select> -->
+                <div class="form-check form-switch">
+                    <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" name="led_set[]" value="led1">
+                    <label class="form-check-label" for="flexSwitchCheckChecked">LED 1</label>
+                </div>
+                <div class="form-check form-switch">
+                    <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" name="led_set[]" value="led2">
+                    <label class="form-check-label" for="flexSwitchCheckChecked">LED 2</label>
+                </div>
+                <div class="form-check form-switch">
+                    <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" name="led_set[]" value="led3">
+                    <label class="form-check-label" for="flexSwitchCheckChecked">LED 3  </label>
+                </div>
             </div>
             
             <input type="datetime-local" id="datetime" name="datetime">
